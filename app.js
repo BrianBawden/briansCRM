d78c09c9f27b05222bff870315303210ae2ef208
Brian Bawden <brianbawden001@gmail.com>
2024-02-07 22:03:25 -0500

    app.js has everything and is pointed to routes.

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongodb = require('./db/connect')
+const dotenv = require("dotenv")
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+dotenv.config()
+
+const port = process.env.PORT || 3000
+const app = express()
+
+app
+  .use(bodyParser.json())
+  .use(cors('*'))
+  .use((req, res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    next()
+  })
+  .use('/', require('./routes'))
+
+  mongodb.initDb((err, mongodb) =>{
+    if(err) {
+      console.log(`Error init DB: ${err}`)
+    } else {
+      app.listen(port)
+      console.log(`DB connected at ${port}`)
+    }
+  })
