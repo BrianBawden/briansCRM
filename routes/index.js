c6dbf68eaf0de0286812197145f71e91c6bda8de
Brian Bawden <brianbawden001@gmail.com>
2024-02-20 09:55:38 -0500

    home page shows login html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 
 const express = require("express")
+const path = require('path')
 const router = express.Router()
 
 router.use("/customers", require("./customers"))
 router.use("/", require("./swagger"))
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/login.html'))
+})
 
 module.exports = router
 
