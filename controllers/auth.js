b9705561587b33e281f65865d9fed0815fb10402
Brian Bawden <brianbawden001@gmail.com>
2024-03-02 23:51:57 -0500

    add render to url.

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,7 @@ dotenv.config()
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/google/callback",  
+    callbackURL: "https://brianscrm.onrender.com/google/callback",  
     passReqToCallback   : true
   },
 
